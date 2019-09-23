bd777c035b21371c661b7b6cff48f132d1918ea2
ZhzeZYi <53176472+ZhzeZYi@users.noreply.github.com>
2019-09-23 16:37:55 +0800

    Add files via upload

---
+++ GitDemo/src/com/yizheze/entity/Student.java
@@ -0,0 +1,10 @@
+package com.yizheze.entity;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("的设计大赛");
+		System.out.println("的设计大赛");
+		System.out.println("的设计大赛");
+	}
+}
